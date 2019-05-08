ddf6874a53f572b1024d221e536dec91f0da821e
David Laubenstein <david.laubenstein@posteo.de>
2019-05-08 13:15:50 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "NotificationBanner",
+    // platforms: [.iOS("10.0"),
+    products: [
+        .library(name: "NotificationBanner", targets: ["NotificationBanner"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .branch("develop")),
+        .package(url: "https://github.com/cbpowell/MarqueeLabel.git", .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "NotificationBanner",
+            dependencies: ["SnapKit", "MarqueeLabel"],
+            path: "NotificationBanner"
+        )
+    ]
+)
